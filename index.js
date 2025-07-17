9bf171ea65015c25b9741625bb5cadd062295004
Joel Eguiza <joeleg96@gmail.com>
2025-07-17 11:30:04 -0600

    1. Improved margins and padding on all pages 2. Completed layout for Technology page 3. Added JS functionality for Technology page 4. Added active states for all pages

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ $(".destination-option").on("click", function(event) {
 // Crew
 $(".gallery-btn").on("click", function(event) {
     $(".crew-container").addClass("hide");
+    $(".gallery-btn").removeClass("gallery-btn-selected");
     $("." + this.id + "-container").removeClass("hide");
+    $("." + this.id + "-gallery-btn").addClass("gallery-btn-selected");
 });
 
+// Technology
+$(".page-number-btn").on("click", function(event) {
+    $(".technology-container").addClass("hide");
+    $(".page-number-btn").removeClass("page-number-btn-selected");
+    $("." + this.id + "-container").removeClass("hide");
+    $("." + this.id + "-btn").addClass("page-number-btn-selected");
+})
